2961ba627ad0979dd633df3393da03fa8541b33d
shubham kumar <sk0228389@gmail.com>
2024-06-06 23:13:10 +0530

    home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,36 +5,66 @@ const Home = () => {
         <>
             <div className='w-full h-lvh flex flex-row' style={{ backgroundImage: "url('https://img.freepik.com/free-vector/bokeh-defocused-background_23-2148497833.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', }}>
                 <div className="cardContainer1 w-1/3 h-lvh relative flex flex-col justify-evenly">
-                    <div className="row1 relative ml-3 h-20 w-4/5">
+                    <div className="row1 relative h-1/2 w-full mt-6">
                         <img
                             className="absolute top-0 left-0 h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-10 h-20 w-1/2"
+                            className="absolute top-[2rem] h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-20  h-20 w-1/2"
+                            className="absolute top-[4rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[6rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[8rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[10rem]  h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                     </div>
-                    <div className="row2 relative  ml-3 h-20 w-4/5">
+                    <div className="row2 relative h-1/2 w-full mt-16">
                         <img
                             className="absolute top-0 left-0 h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-10 h-20 w-1/2"
+                            className="absolute top-[2rem] h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-20  h-20 w-1/2"
+                            className="absolute top-[4rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[6rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[8rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[10rem]  h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
@@ -58,37 +88,67 @@ const Home = () => {
                         <img className="h-1/2 absolute ml-[18rem] mt-[9.5rem]" src="https://images.pexels.com/photos/7600368/pexels-photo-7600368.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                     </div>
                 </div>
-                <div className="cardContainer3 w-1/3 h-lvh relative flex flex-col justify-evenly">
-                    <div className="row1 relative ml-3 h-20 w-4/5">
+                <div className="cardContainer3 w-1/3 h-lvh relative flex flex-col justify-between">
+                    <div className="row1 relative  mt-6 h-1/2 w-full flex justify-end">
                         <img
-                            className="absolute top-0 left-0 h-20 w-1/2"
+                            className="absolute top-[0rem] h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-10 h-20 w-1/2"
+                            className="absolute top-[2rem] h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-20  h-20 w-1/2"
+                            className="absolute top-[4rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[6rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[8rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[10rem]  h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                     </div>
-                    <div className="row2 relative  ml-3 h-20 w-4/5">
+                    <div className="row2 relative mt-16 h-1/2 w-full flex justify-end">
                         <img
-                            className="absolute top-0 left-0 h-20 w-1/2"
+                            className="absolute top-[0rem]  h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-10 h-20 w-1/2"
+                            className="absolute top-[2rem] h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
                         <img
-                            className="absolute top-20  h-20 w-1/2"
+                            className="absolute top-[4rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[6rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[8rem]  h-20 w-1/2"
+                            src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
+                            alt="Canadian Flag"
+                        />
+                        <img
+                            className="absolute top-[10rem]  h-20 w-1/2"
                             src="https://media.istockphoto.com/id/165028040/vector/canadian-flag-vector.jpg?s=612x612&w=0&k=20&c=UAovew36OC60VVtij55DGwpuRYm8l_c7JWdCnt5ZN7A="
                             alt="Canadian Flag"
                         />
